cf13f43f875b73b442c66d107603ed526a338794
Felipe Souza <felipelsouza.dev@gmail.com>
2020-11-19 18:27:17 -0300

    Diplay grid

---
+++ src/components/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="grid"></div>
+</template>
+
+<script></script>
+
+<style scoped>
+#grid {
+    display: grid;
+    height: 100vh;
+
+    grid-template-columns: 230px auto;
+    grid-template-rows: 350px auto 115px;
+
+    /*  OL = Options list
+        HP = Home Page
+        PL = PlayLists
+        MP = Music Player
+    */
+    grid-template-areas: 
+        "OL HP"
+        "PL HP"
+        "MP MP" 
+    ;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+
+import Layout from './components/Layout';
+
+import './styles/main.scss';
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(Layout)
 })
